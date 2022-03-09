cbfac346406e56b292c481c5021399a55920823b
salima5214 <mingming5214@gmail.com>
2022-03-10 05:56:45 +0800

    add voice and exam

--- stages/stage_1/exam.js
+++ stages/stage_1/exam.js
@@ -1,7 +1,18 @@
     // count = 0
-    const getValueInput = () =>{
+    const getValueInput_1_4 = () =>{
         let inputValue = document.getElementById("domTextElement").value;
         if (inputValue == "ma"){
+            document.getElementById("valueInput").innerHTML = "O"; 
+        }else{
+            document.getElementById("valueInput").innerHTML = "X";   
+        }
+
+      }
+
+
+    const getValueInput_2_4 = () =>{
+        let inputValue = document.getElementById("domTextElement").value;
+        if (inputValue == "se"){
             document.getElementById("valueInput").innerHTML = "O"; 
         }else{
             document.getElementById("valueInput").innerHTML = "X";   
