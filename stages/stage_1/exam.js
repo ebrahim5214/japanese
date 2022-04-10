fce2d5e6e74fb1eb18bbced5b72263d83d03ff4a
salima5214 <mingming5214@gmail.com>
2022-04-11 04:59:53 +0800

    金魂師: 加入第二關戰鬥

--- stages/stage_1/exam.js
+++ stages/stage_1/exam.js
@@ -279,6 +279,261 @@ function start_1_10(){
         timer.innerText = number + 0 }, 1000);
     }
 
+// 挑戰--------------------------------------------------------
+
+function start_3_10(){
+    //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+        var timer = document.querySelector("#timer");
+        var number = 15;
+        setInterval(function(){
+        number -- ;
+        if(number <= 0 ){
+            number = 0;
+            javascript:window.location.href='learn_3_9_entry.html';}
+            
+        timer.innerText = number + 0 }, 1000);
+    }
+
+
+    
+const getValueInput_3_10 = () =>{
+        let inputValue = document.getElementById("domTextElement").value;
+        if (inputValue == "gi"){
+            var audio = document.createElement("audio");
+            audio.src = "../../media/music/true.m4a";
+            audio.playbackRate = 1.7;audio.play();
+            document.getElementById("valueInput").innerHTML = "答對了! 前進下一關!"; 
+            setTimeout(function(){ javascript:window.location.href='learn_3_11.html'; }, 2000);    
+            correct_count_stage_1_1 += 1;
+        }else{ 
+            var audio = document.createElement("audio");
+            audio.src = "../../media/music/false.m4a";
+            audio.playbackRate = 1.7;audio.play();
+            document.getElementById("valueInput").innerHTML = "答錯了!請重新挑戰!";  
+            setTimeout(function(){ javascript:window.location.href='learn_3_9_entry.html'; }, 2000); 
+        }
+
+}
+
+
+// 挑戰--------------------------------------------------------
+
+function start_3_11(){
+    //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+        var timer = document.querySelector("#timer");
+        var number = 15;
+        setInterval(function(){
+        number -- ;
+        if(number <= 0 ){
+            number = 0;
+            javascript:window.location.href='learn_3_9_entry.html';}
+            
+        timer.innerText = number + 0 }, 1000);
+    }
+
+
+    
+const getValueInput_3_11 = () =>{
+        let inputValue = document.getElementById("domTextElement").value;
+        if (inputValue == "zi"){
+            var audio = document.createElement("audio");
+            audio.src = "../../media/music/true.m4a";
+            audio.playbackRate = 1.7;audio.play();
+            document.getElementById("valueInput").innerHTML = "答對了! 前進下一關!"; 
+            setTimeout(function(){ javascript:window.location.href='learn_3_12.html'; }, 2000);    
+            correct_count_stage_1_1 += 1;
+        }else{ 
+            var audio = document.createElement("audio");
+            audio.src = "../../media/music/false.m4a";
+            audio.playbackRate = 1.7;audio.play();
+            document.getElementById("valueInput").innerHTML = "答錯了!請重新挑戰!";  
+            setTimeout(function(){ javascript:window.location.href='learn_3_9_entry.html'; }, 2000); 
+        }
+
+}
+
+
+// 挑戰--------------------------------------------------------
+
+function start_3_12(){
+    //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+        var timer = document.querySelector("#timer");
+        var number = 15;
+        setInterval(function(){
+        number -- ;
+        if(number <= 0 ){
+            number = 0;
+            javascript:window.location.href='learn_3_9_entry.html';}
+            
+        timer.innerText = number + 0 }, 1000);
+    }
+
+
+    
+const getValueInput_3_12 = () =>{
+        let inputValue = document.getElementById("domTextElement").value;
+        if (inputValue == "pu"){
+            var audio = document.createElement("audio");
+            audio.src = "../../media/music/true.m4a";
+            audio.playbackRate = 1.7;audio.play();
+            document.getElementById("valueInput").innerHTML = "答對了! 前進下一關!"; 
+            setTimeout(function(){ javascript:window.location.href='learn_3_13.html'; }, 2000);    
+            correct_count_stage_1_1 += 1;
+        }else{ 
+            var audio = document.createElement("audio");
+            audio.src = "../../media/music/false.m4a";
+            audio.playbackRate = 1.7;audio.play();
+            document.getElementById("valueInput").innerHTML = "答錯了!請重新挑戰!";  
+            setTimeout(function(){ javascript:window.location.href='learn_3_9_entry.html'; }, 2000); 
+        }
+
+}
+
+// 挑戰2--------------------------------------------------------
+
+function start_3_12(){
+    //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+        var timer = document.querySelector("#timer");
+        var number = 15;
+        setInterval(function(){
+        number -- ;
+        if(number <= 0 ){
+            number = 0;
+            javascript:window.location.href='learn_3_15_entry.html';}
+            
+        timer.innerText = number + 0 }, 1000);
+    }
+
+// 挑戰--------------------------------------------------------
+
+function start_4_14(){
+    //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+        var timer = document.querySelector("#timer");
+        var number = 15;
+        setInterval(function(){
+        number -- ;
+        if(number <= 0 ){
+            number = 0;
+            javascript:window.location.href='learn_4_13_entry.html';}
+            
+        timer.innerText = number + 0 }, 1000);
+    }
+
+
+    
+const getValueInput_4_14 = () =>{
+        let inputValue = document.getElementById("domTextElement").value;
+        if (inputValue == "pyu"){
+            var audio = document.createElement("audio");
+            audio.src = "../../media/music/true.m4a";
+            audio.playbackRate = 1.7;audio.play();
+            document.getElementById("valueInput").innerHTML = "答對了! 前進下一關!"; 
+            setTimeout(function(){ javascript:window.location.href='learn_4_15.html'; }, 2000);    
+            correct_count_stage_1_1 += 1;
+        }else{ 
+            var audio = document.createElement("audio");
+            audio.src = "../../media/music/false.m4a";
+            audio.playbackRate = 1.7;audio.play();
+            document.getElementById("valueInput").innerHTML = "答錯了!請重新挑戰!";  
+            setTimeout(function(){ javascript:window.location.href='learn_4_13_entry.html'; }, 2000); 
+        }
+
+}
+
+const getValueInput_4_15 = () =>{
+    let inputValue = document.getElementById("domTextElement").value;
+    if (inputValue == "rya"){
+        var audio = document.createElement("audio");
+        audio.src = "../../media/music/true.m4a";
+        audio.playbackRate = 1.7;audio.play();
+        document.getElementById("valueInput").innerHTML = "答對了! 前進下一關!"; 
+        setTimeout(function(){ javascript:window.location.href='learn_4_16.html'; }, 2000);    
+        correct_count_stage_1_1 += 1;
+    }else{ 
+        var audio = document.createElement("audio");
+        audio.src = "../../media/music/false.m4a";
+        audio.playbackRate = 1.7;audio.play();
+        document.getElementById("valueInput").innerHTML = "答錯了!請重新挑戰!";  
+        setTimeout(function(){ javascript:window.location.href='learn_4_13_entry.html'; }, 2000); 
+    }
+
+}
+
+const getValueInput_4_16 = () =>{
+    let inputValue = document.getElementById("domTextElement").value;
+    if (inputValue == "gyo"){
+        var audio = document.createElement("audio");
+        audio.src = "../../media/music/true.m4a";
+        audio.playbackRate = 1.7;audio.play();
+        document.getElementById("valueInput").innerHTML = "答對了! 前進下一關!"; 
+        setTimeout(function(){ javascript:window.location.href='learn_4_17.html'; }, 2000);    
+        correct_count_stage_1_1 += 1;
+    }else{ 
+        var audio = document.createElement("audio");
+        audio.src = "../../media/music/false.m4a";
+        audio.playbackRate = 1.7;audio.play();
+        document.getElementById("valueInput").innerHTML = "答錯了!請重新挑戰!";  
+        setTimeout(function(){ javascript:window.location.href='learn_4_13_entry.html'; }, 2000); 
+    }
+
+}
+
+// 挑戰2--------------------------------------------------------
+
+function start_4_20(){
+    //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+        var timer = document.querySelector("#timer");
+        var number = 15;
+        setInterval(function(){
+        number -- ;
+        if(number <= 0 ){
+            number = 0;
+            javascript:window.location.href='learn_4_19_entry.html';}
+            
+        timer.innerText = number + 0 }, 1000);
+    }
+
+    // 挑戰2--------------------------------------------------------
+
+function start_5_6(){
+    //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+        var timer = document.querySelector("#timer");
+        var number = 15;
+        setInterval(function(){
+        number -- ;
+        if(number <= 0 ){
+            number = 0;
+            javascript:window.location.href='learn_5_5_entry.html';}
+            
+        timer.innerText = number + 0 }, 1000);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
     function my_true(){
     var audio = document.createElement("audio");
             audio.src = "../../media/music/true.m4a";
