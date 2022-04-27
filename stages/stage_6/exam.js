510bed4fa51df8f87b38cbde5d8ef183fd6986cd
salima5214 <mingming5214@gmail.com>
2022-04-27 17:13:50 +0800

    remove single final fight

---
+++ stages/stage_6/exam.js
@@ -0,0 +1,88 @@
+
+
+// 挑戰--------------------------------------------------------
+
+function start_stage_2_1_6(){
+    //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+        var timer = document.querySelector("#timer");
+        var number = 15;
+        setInterval(function(){
+        number -- ;
+        if(number <= 0 ){
+            number = 0;
+            javascript:window.location.href='learn_1_5_entry.html';}
+            
+        timer.innerText = number + 0 }, 1000);
+    }
+
+
+    
+function start_stage_2_2_6(){
+    //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+        var timer = document.querySelector("#timer");
+        var number = 15;
+        setInterval(function(){
+        number -- ;
+        if(number <= 0 ){
+            number = 0;
+            javascript:window.location.href='learn_2_5_entry.html';}
+            
+        timer.innerText = number + 0 }, 1000);
+    }
+
+    function start_stage_2_3_6(){
+        //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+            var timer = document.querySelector("#timer");
+            var number = 15;
+            setInterval(function(){
+            number -- ;
+            if(number <= 0 ){
+                number = 0;
+                javascript:window.location.href='learn_3_5_entry.html';}
+                
+            timer.innerText = number + 0 }, 1000);
+        }
+    
+        function start_stage_2_4_6(){
+            //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+                var timer = document.querySelector("#timer");
+                var number = 15;
+                setInterval(function(){
+                number -- ;
+                if(number <= 0 ){
+                    number = 0;
+                    javascript:window.location.href='learn_4_5_entry.html';}
+                    
+                timer.innerText = number + 0 }, 1000);
+            }
+    
+            function start_stage_2_5_6(){
+                //   按下 start 後 id 為 timer 的 DIV 內容可以開始倒數到到 0。 
+                    var timer = document.querySelector("#timer");
+                    var number = 15;
+                    setInterval(function(){
+                    number -- ;
+                    if(number <= 0 ){
+                        number = 0;
+                        javascript:window.location.href='learn_5_5_entry.html';}
+                        
+                    timer.innerText = number + 0 }, 1000);
+                }
+
+
+    function my_true(){
+    var audio = document.createElement("audio");
+            audio.src = "../../media/music/true.m4a";
+            audio.playbackRate = 1.7;audio.play();
+            // setTimeout(function(){ javascript:window.location.href='learn_1_9_entry.html'; }, 1000); 
+            // javascript:window.location.href='learn_1_9_entry.html';
+
+    }
+
+
+    function my_false(){
+        var audio = document.createElement("audio");
+                audio.src = "../../media/music/false.m4a";
+                audio.playbackRate = 1.7;audio.play();
+    
+        }
